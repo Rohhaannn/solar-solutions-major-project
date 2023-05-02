56947dbb21f7d255ce16b100b8675d996a25bf08
CarryAllen <rohanshingade1095@gmail.com>
2023-05-02 19:55:53 +0530

    express-session module added

--- 28-04-2023_Major_Project/index.js
+++ 28-04-2023_Major_Project/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var ejs = require('ejs'); //which is gonna allow me pass data to HTML whenever I want
 var bodyparser = require('body-parser');
 var mysql = require('mysql');
+var session = require('express-session');
+
 
 mysql.createConnection({
   host: "localhost",
@@ -17,6 +19,8 @@ app.set('view engine', 'ejs');
 
 app.listen(3000);   //port number
 app.use(bodyparser.urlencoded({extended: true}));
+app.use(session({secret:"secret"}));
+
 
 //localhost:3000
 app.get('/', function(req, res) {
