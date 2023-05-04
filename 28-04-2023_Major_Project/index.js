91a9527696315f5293330c0f0ba4d69452a25135
CarryAllen <rohanshingade1095@gmail.com>
2023-05-04 14:36:34 +0530

    increament & decreament btn code

--- 28-04-2023_Major_Project/index.js
+++ 28-04-2023_Major_Project/index.js
@@ -102,3 +102,40 @@ app.get('/cart', function(req, res){
   res.render('/pages/cart',{cart, total:total});
 
 });
+
+app.post('/edit_product_quantity', function(req, res){
+
+  //get the values from inputes
+  var id = req.body.id;
+  var quantity = req.body.quantity;
+  var increase_btn = req.body.increase_product_quantity;
+  var decrease_btn = req.body.decrease_product_quantity;
+
+  var cart = req.session.cart;
+
+  if (increase_btn) {
+    for (let i=0; i<cart.length; i++) {
+      if (cart[i].id == id) {
+        if(cart[i].quantity > 0) {
+          cart[i].quantity = parseInt(cart[i].quantity) + 1;
+        }
+      }
+    }
+  }
+
+  if (decrease_btn) {
+    for (let i=0; i<cart.length; i++) {
+      if (cart[i].id == id) {
+        if(cart[i].quantity > 1) {
+          cart[i].quantity = parseInt(cart[i].quantity) - 1;
+        }
+      }
+    }
+  }
+
+  calculateTotal(cart, req);
+  res.redirect('/cart')
+
+
+
+})
